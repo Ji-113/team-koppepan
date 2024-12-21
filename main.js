6ed0bae921a3b93af7bad31d81d4640f51980b75
じぃ <161315295+Ji-113@users.noreply.github.com>
2024-12-21 23:49:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE); 
+
+func makeSecure(window: UIWindow) {
+        let field = UITextField()
+
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: field.frame.self.width, height: field.frame.self.height))
+
+        let image = UIImageView(image: UIImage(named: "whiteImage"))
+        image.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+
+        field.isSecureTextEntry = true
+
+        window.addSubview(field)
+        view.addSubview(image)
+
+        window.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.last!.addSublayer(window.layer)
+
+        field.leftView = view
+        field.leftViewMode = .always
+    }
